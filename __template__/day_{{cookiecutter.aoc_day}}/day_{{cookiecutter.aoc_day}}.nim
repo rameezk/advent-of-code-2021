41843a730427fe09cb766f642da97eff35c85a29
Rameez Khan <rameezkhan.sa@gmail.com>
2021-11-24 09:08:19 +0200

    Update template

--- __template__/day_{{cookiecutter.aoc_day}}/day_{{cookiecutter.aoc_day}}.nim
+++ __template__/day_{{cookiecutter.aoc_day}}/day_{{cookiecutter.aoc_day}}.nim
@@ -1,3 +1,6 @@
-let input = readFile("./input.txt")
+import strutils, sequtils, sugar
+
+let input = readFile("./input.txt").splitLines.filter((line) => not isEmptyOrWhitespace(line)).map(parseInt)
+# let input: seq = @[1, 2, 3]
 
 echo input
